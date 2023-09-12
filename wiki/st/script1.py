5acd95da66f955582e3597db1c61ce04c7496303
facahto <72359515+facahto@users.noreply.github.com>
2023-09-13 01:06:22 +0700

    Add files via upload

---
+++ wiki/st/script1.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.write(f'''
+        <a target="_self" href="https://nengtiwi-mjn4wrp3pmjb7cvaqdv3ls.streamlit.app/">
+            <button>
+                Kembali Ke Halaman Utama
+            </button>
+        </a>
+        ''',
+        unsafe_allow_html=True)   
+st.write('')
+st.write('')
+st.write("Baik, saya coba perhitungkan simulasi kreditnya, ya pak/bu. Untuk mengetahui harga mobil, boleh diinformasikan KTP dari daerah mana Bapak/Ibu?")
+dom = st.text_input('Asal KTP')
+st.write('---')
+st.write("Baik, untuk tenor rencana mau yang berapa tahun, pak/bu?")
+tenor = st.text_input('Tenor')
+st.write('---')
+st.write("Baik, untuk DP rencana mau yang berapa persen, pak/ibu? atau mau coba DP minimal kami sebesar 20%?")
+DP = st.text_input('Persenan DP')
+st.write('---')
+st.write('Baik, untuk pembayaran pertamanya mau ADDM atau ADDB?')
+bayar = st.text_input('pembayaran pertama (ADDM/ADDB)')
+st.write('---')
+st.write('Apakah bapak/ibu bersedia menunggu?')
+l1, l2,l3,l4 = st.columns(4)
+agree = l1.button('Menunggu')
+donta = l2.button('via Whatsapp')
+if agree:
+    st.write('Baik Bapak/Ibu, saya simulasikan perhitungan kreditnya terlebih dahulu. Mohon ditunggu sebentar.')
+if donta:
+    st.write('Baik Bapak/Ibu, saya simulasikan perhitungan kreditnya terlebih dahulu. Mohon ditunggu, saya akan segera kirimkan simulasi perhitungan kredit melalui nomor Whatsapp Bapak/Ibu yang saat ini saya hubungi. Terima kasih dan sehat selalu pak/bu')
+
